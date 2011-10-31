f67d7f94ad258342990c6db5ff914fe510fc8294
Nick Clayton <southstandblue@yahoo.co.uk>
2011-10-31 14:48:04 +0000

    first commit

---
+++ src/com/knightfinderapp/KnightFinderActivity.java
@@ -0,0 +1,13 @@
+package com.knightfinderapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KnightFinderActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
